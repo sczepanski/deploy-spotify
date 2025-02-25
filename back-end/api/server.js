06876b8173004d13d9c3e7cd323c0ee9e8f51e03
Eduardo Sczepanski <eduardo.sczepanski@gmail.com>
2025-02-25 15:36:34 -0300

    Ajustes para o Deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,7 +1,7 @@
 // API signific Application Programming Interface
 // POST, GET, PUT, DELETE
 // CRUD - Create Read Update Delete
-// Endpoint, uma rota que pode ser acessada atráves de uma API = "/"
+// Endpoint, uma rota que pode ser acessada atráves de uma API = "/api/"
 // node --watch ele observa se houve alguma atualização durante o servidor que está ligado.
 // Sempre que for importar o "express", incluir o método de importação mais recente dentro do package.json,
 // que seria: "type": "module",
@@ -10,6 +10,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path, { join } from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -17,18 +20,24 @@ const PORT = 3000;
 app.use(cors()); // permite requisição de qualquer origem
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
 // Toda vez que usar o await dentro de uma função, também tenho que chamar o async
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist/")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
